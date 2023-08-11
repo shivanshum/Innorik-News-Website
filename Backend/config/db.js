4d4fd824fb95c11e191212aa2d9eec3301d22922
SHIVANSHU MISHRA <shivanshum354@gmail.com>
2023-08-11 22:17:18 +0530

    vercel

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 
 const connection = mongoose.connect(process.env.inUrl)
-
+inUrl = "https://innorik-news-website-backend-shivanshum.vercel.app/";
 module.exports = {
     connection
 }
